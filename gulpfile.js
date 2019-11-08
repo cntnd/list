fe61e15779564c52f7926333b48b434cc5ada96c
Thomas D'Ascoli <thomas@dasco.li>
2019-11-08 20:49:15 +0100

    add dist gulp task and zip src folder (except scss)

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp        = require('gulp');
 var sass        = require('gulp-sass');
 var minify      = require('gulp-minifier');
+var zip         = require('gulp-zip');
 
 gulp.task('watch', function () {
     gulp.watch("src/scss/**/*.scss", gulp.series('sass'));
@@ -8,7 +9,7 @@ gulp.task('watch', function () {
 
 // Compile sass into CSS
 gulp.task('sass', function() {
-    return gulp.src("src/scss/*.scss")
+    return gulp.src("src/scss/**/*.scss")
         .pipe(sass())
         .pipe(minify({
           minify: true,
@@ -21,4 +22,12 @@ gulp.task('sass', function() {
         .pipe(gulp.dest("src/css/"));
 });
 
+gulp.task('zip', function() {
+  return gulp.src(['src/**/*','!src/scss/'])
+  		.pipe(zip('cntnd_list.zip'))
+  		.pipe(gulp.dest('dist'));
+});
+
 gulp.task('default', gulp.series('sass','watch'));
+
+gulp.task('dist', gulp.series('sass','zip'));
